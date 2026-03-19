c5ad84ecff57c9d1c9f1731dd32f0c190d4e45f0
ch0zy <99010854+ch0zy@users.noreply.github.com>
2026-03-19 16:38:15 +0800

    Delete v2 directory

--- v2/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import ChatUI from './ChatUI.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ChatUI />
-  </StrictMode>
-)
